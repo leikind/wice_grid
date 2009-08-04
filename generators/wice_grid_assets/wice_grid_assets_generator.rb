e3f1345ddc7c7cd66ebe435ee29c0a98bab44308
Yuri Leikind <yuri.leikind@gmail.com>
2009-08-04 16:54:10 +0200

    assets generator added

---
+++ generators/wice_grid_assets/wice_grid_assets_generator.rb
@@ -0,0 +1,26 @@
+class WiceGridAssetsGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      # wice_grid config
+      m.directory "config/initializers"
+      m.file "initializers/wice_grid_config.rb",  "config/initializers/wice_grid_config.rb"
+
+      # wice_grid js & css
+      m.file "javascripts/wice_grid.js",  "public/javascripts/wice_grid.js"
+      m.file "stylesheets/wice_grid.css",  "public/stylesheets/wice_grid.css"
+
+      # calendarview js & css
+      m.file "javascripts/calendarview.js",  "public/javascripts/calendarview.js"
+      m.file "stylesheets/calendarview.css",  "public/stylesheets/calendarview.css"
+
+      # images
+      m.directory "public/images/icons/grid"
+      
+      %w(arrow_down.gif calendar_view_month.png expand.png page_white_find.png table_refresh.png  
+        arrow_up.gif delete.png page_white_excel.png    table.png).each do |f|
+          m.file "icons/#{f}",  "public/images/icons/grid/#{f}"
+      end
+      
+    end
+  end
+end
