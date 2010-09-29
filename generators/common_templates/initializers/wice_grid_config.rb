369f0f41b253839be3bd7bec82e488c03d579b6b
Yuri Leikind <yuri@pogio.net>
2010-09-29 11:35:13 +0200

    fix from iva2k: parsing time with Time.zone instead of  DateTime. Requires the config file to be updated

--- generators/common_templates/initializers/wice_grid_config.rb
+++ generators/common_templates/initializers/wice_grid_config.rb
@@ -130,15 +130,15 @@ if defined?(Wice::Defaults)
 
   # With Calendar helpers enabled the parameter sent is the string displayed. This lambda will be given a date string in the
   # format defined by +DATETIME_FORMAT+ and must generate a DateTime object.
-  # In many cases <tt>DateTime.parse</tt> is enough, for instance,  <tt>%Y-%m-%d</tt>. If you change the format, make sure to check this code
+  # In many cases <tt>Time.zone.parse</tt> is enough, for instance,  <tt>%Y-%m-%d</tt>. If you change the format, make sure to check this code
   # and modify it if needed.
-  Wice::Defaults::DATETIME_PARSER = lambda{|datetime_string| DateTime.parse(datetime_string) }
+  Wice::Defaults::DATETIME_PARSER = lambda{|datetime_string| Time.zone.parse(datetime_string) }
 
   # With Calendar helpers enabled the parameter sent is the string displayed. This lambda will be given a date string in the
   # format defined by +DATETIME+ and must generate a Date object.
   # In many cases <tt>Date.parse</tt> is enough, for instance,  <tt>%Y-%m-%d</tt>. If you change the format, make sure to check this code
   # and modify it if needed.
-  Wice::Defaults::DATE_PARSER = lambda{|datetime_string| Date.parse(datetime_string) }
+  Wice::Defaults::DATE_PARSER = lambda{|date_string| Date.parse(date_string) }
 
   # Icon to popup the calendar.
   Wice::Defaults::CALENDAR_ICON = "/images/icons/grid/calendar_view_month.png"
