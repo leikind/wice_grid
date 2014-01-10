039d692190b9cc5c4d4a05f6eeb8827dca358d9a
Yuri Leikind <yuri.leikind@gmail.com>
2014-01-10 15:07:56 +0100

    better handling of conditions for datetime filters

--- lib/wice/columns/column_datetime.rb
+++ lib/wice/columns/column_datetime.rb
@@ -109,12 +109,12 @@ module Wice
         conditions = [[]]
         if opts[:fr]
           conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} >= ? "
-          conditions << opts[:fr]
+          conditions << opts[:fr].to_date
         end
 
         if opts[:to]
-          conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} <= ? "
-          conditions << opts[:to]
+          conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} < ? "
+          conditions << (opts[:to].to_date + 1)
         end
 
         return false if conditions.size == 1
