a096bd41a8321079104758a0315a48ccfc1bf61d
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-07-16 13:14:55 +0200

    Fix.

--- spec/support/active_record.rb
+++ spec/support/active_record.rb
@@ -4,6 +4,10 @@ ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
 
 load 'spec/schema.rb'
 
+class ActiveRecord::ConnectionAdapters::Column
+  attr_accessor :model
+end
+
 class Dummy < ActiveRecord::Base
 end
 
