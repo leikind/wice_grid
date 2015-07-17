d670c26d03791e90f02bfb6534684dae96fe0c6d
Guillermo Guerrero Ibarra <wolf.fox1985@gmail.com>
2015-07-17 09:41:05 +0200

    Added table_column_matrix test.

--- spec/wice/lib/table_column_matrix_spec.rb
+++ spec/wice/lib/table_column_matrix_spec.rb
@@ -19,6 +19,21 @@ module Wice
     it 'should set default_model_class' do
       table = TableColumnMatrix.new
       table.default_model_class = Dummy
+
+      expect(table.class).to eq(TableColumnMatrix)
+    end
+
+    it 'should get column' do
+      table = TableColumnMatrix.new
+      table.default_model_class = Dummy
+
+      expect(Dummy.columns).to include(table.get_column_by_model_class_and_column_name(Dummy, :name))
+      expect(Dummy.columns).to include(table.get_column_in_default_model_class_by_column_name(:name))
+    end
+
+    it 'should get column not initialized' do
+      table = TableColumnMatrix.new
+      expect {table.get_column_in_default_model_class_by_column_name(:name) }.to raise_error
     end
   end
 end
