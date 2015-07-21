794ca94bb9d3518fc4b3cb3133636c8ca05e2f5e
Yuri Lekind <yuri.leikind@gmail.com>
2015-07-21 08:31:27 +0200

    fix

--- lib/wice/columns/common_rails_date_datetime_conditions_generator_mixin.rb
+++ lib/wice/columns/common_rails_date_datetime_conditions_generator_mixin.rb
@@ -4,28 +4,15 @@ module Wice
     module CommonJsDateDatetimeConditionsGeneratorMixin #:nodoc:
 
       def generate_conditions(table_alias, opts)   #:nodoc:
-
-        datetime = @column_type == :datetime || @column_type == :timestamp
-
         conditions = [[]]
         if opts[:fr]
           conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} >= ? "
-          date = opts[:fr].to_date
-          if datetime
-            date = date.to_datetime
-          end
-          conditions << date
+          conditions << opts[:fr].to_date
         end
 
         if opts[:to]
-          op = '<='
-          date = opts[:to].to_date
-          if datetime
-            date = (date + 1).to_datetime
-            op = '<'
-          end
-          conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} #{op} ? "
-          conditions << date
+          conditions[0] << " #{@column_wrapper.alias_or_table_name(table_alias)}.#{@column_wrapper.name} <= ? "
+          conditions << (opts[:to].to_date + 1)
         end
 
         return false if conditions.size == 1
