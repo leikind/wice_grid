3318dc0b98113b152714359f24520b90216cf656
Jan Rieger <jan.rieger@ad-notam.com>
2015-09-22 16:25:31 +0200

    move locale files to `config/locales`
    This way Rails can autoload them so there is no need to copy the
    translation file on install or updates

--- lib/generators/wice_grid/install_generator.rb
+++ lib/generators/wice_grid/install_generator.rb
@@ -2,15 +2,12 @@
 module WiceGrid #:nodoc:
   module Generators #:nodoc:
     class InstallGenerator < Rails::Generators::Base #:nodoc:
-      desc 'Copy WiceGrid wice_grid_config.rb to config/initializers, ' \
-        'wice_grid.yml to config/locales/, and wice_grid.scss to assets/stylesheets'
+      desc 'Copy WiceGrid wice_grid_config.rb to config/initializers'
 
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_stuff #:nodoc:
         template 'wice_grid_config.rb', 'config/initializers/wice_grid_config.rb'
-
-        copy_file 'wice_grid.yml',  'config/locales/wice_grid.yml'
       end
     end
   end
