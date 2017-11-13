9d488297efe64fd9b422d1691cdc32aef3254b3b
Vladimir Panarkin <vpanarkin@gmail.com>
2017-11-14 00:53:13 +0300

    Fix tag_options

--- lib/wice/wice_grid_core_ext.rb
+++ lib/wice/wice_grid_core_ext.rb
@@ -113,7 +113,11 @@ module ActionView #:nodoc:
   module Helpers #:nodoc:
     module TagHelper #:nodoc:
       def public_tag_options(options, escape = true) #:nodoc:
-        tag_options(options, escape)
+        if respond_to? :tag_options
+          tag_options(options, escape)
+        else
+          tag_builder.tag_options(options, escape)
+        end
       end
     end
   end
