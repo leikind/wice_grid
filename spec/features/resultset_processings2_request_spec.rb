3c23ad8a93f9889997d7f030cd72e0efcb9f3ddd
Jason Barnabe <jason@kickbooster.me>
2018-06-01 09:13:38 -0500

    Fix more ID ordering failures

--- spec/features/resultset_processings2_request_spec.rb
+++ spec/features/resultset_processings2_request_spec.rb
@@ -8,33 +8,20 @@ describe 'with_resultset WiceGrid', type: :request, js: true do
 
   it 'should show records displayed on all pages' do
     find(:css, '#process').click
-    sleep 1
-
-    within '.example' do
-      page.should have_content('50 records on all pages: 540, 519, 507, 537, 531, 551, 515, 511, 524, 542, 523, 527, 518, 535, 539, 520, 532, 512, 514, 522, 546, 516, 521, 544, 543, 550, 552, 510, 541, 553, 529, 508, 513, 528, 556, 548, 547, 525, 534, 555, 549, 536, 545, 509, 517, 538, 526, 554, 530, and 533')
-    end
+    page.should have_content('50 records on all pages: 507, 519, 537, 540, 511, 515, 523, 524, 527, 531, 542, 551, 518, 520, 532, 535, 539, 512, 514, 516, 521, 522, 543, 544, 546, 550, 552, 510, 541, 553, 508, 513, 528, 529, 548, 556, 525, 534, 547, 555, 509, 517, 526, 536, 538, 545, 549, 530, 533, and 554')
   end
 
   it 'should show records displayed on all pages with a text filter selection' do
     fill_in('g_f_title', with: 'ed')
     sleep 2
     find(:css, '#process').click
-    sleep 1
-
-    within '.example' do
-      page.should have_content('2 records on all pages: 507 and 534')
-    end
+    page.should have_content('2 records on all pages: 507 and 534')
   end
 
   it 'should show records displayed on all pages with a custom filter selection' do
     select 'Cancelled',  from: 'g_f_status_id'
-    sleep 1
-
+    sleep 2
     find(:css, '#process').click
-    sleep 1
-
-    within '.example' do
-      page.should have_content('8 records on all pages: 531, 551, 515, 511, 524, 542, 523, and 527')
-    end
+    page.should have_content('8 records on all pages: 511, 515, 523, 524, 527, 531, 542, and 551')
   end
 end
