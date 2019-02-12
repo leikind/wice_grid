cb4c44a2e4816e8077fbf66f05a2e567bddcd198
Jason Barnabe <jason@kickbooster.me>
2019-02-12 14:40:51 -0600

    Sleep more

--- spec/features/detached_filters_two_grids_spec.rb
+++ spec/features/detached_filters_two_grids_spec.rb
@@ -31,6 +31,9 @@ describe 'buttons WiceGrid', type: :request, js: true do
     set_datepicker(self, 'grid_f_due_date_to_date_placeholder', 2012, 11, 31)
     find(:css, '.external-buttons-grid1 .wg-external-submit-button').click
 
+    # Test is flaky, not sure why.
+    sleep 1
+
     within 'div#grid.wice-grid-container .pagination_status' do
       expect(page).to have_content('1-3 / 3')
     end
@@ -38,6 +41,8 @@ describe 'buttons WiceGrid', type: :request, js: true do
     set_datepicker(self, 'grid2_f_due_date_fr_date_placeholder', 2013, 0, 1)
     set_datepicker(self, 'grid2_f_due_date_to_date_placeholder', 2013, 11, 31)
     find(:css, '.external-buttons-grid2 .wg-external-submit-button').click
+
+    sleep 1
 
     # stays the same
     within 'div#grid.wice-grid-container .pagination_status' do
@@ -50,6 +55,8 @@ describe 'buttons WiceGrid', type: :request, js: true do
 
     find(:css, '.external-buttons-grid1 .wg-external-reset-button').click
 
+    sleep 1
+
     within 'div#grid.wice-grid-container .pagination_status' do
       expect(page).to have_content('1-20 / 50')
     end
@@ -59,6 +66,8 @@ describe 'buttons WiceGrid', type: :request, js: true do
     end
 
     find(:css, '.external-buttons-grid2 .wg-external-reset-button').click
+
+    sleep 1
 
     within 'div#grid.wice-grid-container .pagination_status' do
       expect(page).to have_content('1-20 / 50')
@@ -72,7 +81,6 @@ describe 'buttons WiceGrid', type: :request, js: true do
 
     find(:css, '.external-buttons-grid1 .wg-external-submit-button').click
 
-    # Test is flaky, not sure why.
     sleep 1
 
     within 'div#grid.wice-grid-container .pagination_status' do
