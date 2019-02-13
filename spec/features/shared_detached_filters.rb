96455891e7515940dd30ec70b79412e8fa513f97
Jason Barnabe <jason@kickbooster.me>
2019-02-13 15:42:18 -0600

    More deflaking

--- spec/features/shared_detached_filters.rb
+++ spec/features/shared_detached_filters.rb
@@ -74,12 +74,13 @@ shared_examples "detached_filters" do
       click_on 'ID'
     end
 
+    within 'div.wice-grid-container table.wice-grid tbody tr:first-child td.active-filter' do
+      expect(page).to have_content('sed impedit iste')
+    end
+
     within '.pagination_status' do
       expect(page).to have_content('1-2 / 2')
     end
-
-    expect(page).to have_content('corporis expedita vel')
-    expect(page).to have_content('sed impedit iste')
 
     click_button('Reset')
     within '.pagination_status' do
